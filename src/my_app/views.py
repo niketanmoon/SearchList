441569bb1bb5e6f6bd5ebe3674064892b35bf496
Niketan E. Moon <niketanmoon@gmail.com>
2019-09-18 02:55:24 +0530

    Added image data ids to render images

--- src/my_app/views.py
+++ src/my_app/views.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 from .models import Search
 
 CRAIGSLIST_URL = 'https://indore.craigslist.org/search/?query={}'
+IMAGE_URL      = 'https://images.craigslist.org/{}_300x300.jpg'
 # Create your views here.
 
 def home(request):
@@ -17,7 +18,6 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
-    Search.objects.create(search = search)
     # formatting the url
     url = CRAIGSLIST_URL.format(quote_plus(search))
     # getting the response from the site through requests
@@ -41,7 +41,16 @@ def new_search(request):
             post_price = post.find(class_='result-price').text
         else:
             post_price = 'N/A'
-        final_postings.append((post_title, post_url, post_price))
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = IMAGE_URL.format(post_image_id)
+            print(post_image_url)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+        final_postings.append((post_title, post_url, post_price, post_image_url))
+
 
 
     front_end = {
